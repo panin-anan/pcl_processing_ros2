b8097879b41ffbf753162ea8927aad18a1de4383
Luka140 <luka-groot@hotmail.nl>
2024-10-25 07:39:16 +0000

    adjusted belt width threshold to fraction in launch file

--- launch/scan_and_process_pcl.launch.py
+++ launch/scan_and_process_pcl.launch.py
@@ -19,16 +19,16 @@ def generate_launch_description():
         executable="pcl_processor",
         name='pcl_process',    # This is the first line in the config file 
         parameters=[
-            {'dist_threshold':       '0.0006',            # Distance to filter grinded area. do not go near #50-80 micron on line axis, 200 micron on feed axis of rate 30 second
-            'cluster_neighbor':               '20',                # filter outlier with #of neighbour point threshold
-            'plate_thickness':                  '0.0023',                # in m
-            'plane_error_allowance':            '5',               #in degree
-            'clusterscan_eps':               '0.00025',              # cluster minimum dist grouping in m
-            'laserline_threshold':           '0.00008',      # scan resolution line axis in m
-            'feedaxis_threshold':            '0.00012',     # scan resolution robot feed axis in m
-            'concave_resolution':             '0.0005',       #concave hull resolution
-            'filter_down_size':                 '0.0002',       #voxel down size on clustering
-            'belt_width_threshold':             '0.04',           #minimum detected belt width in m 
+            {'dist_threshold':              '0.0006',       # Distance to filter grinded area. do not go near #50-80 micron on line axis, 200 micron on feed axis of rate 30 second
+            'cluster_neighbor':             '20',           # filter outlier with #of neighbour point threshold
+            'plate_thickness':              '0.0023',       # in m
+            'plane_error_allowance':        '5',            # degree
+            'clusterscan_eps':              '0.00025',      # cluster minimum dist grouping in m
+            'laserline_threshold':          '0.00008',      # scan resolution line axis in m
+            'feedaxis_threshold':           '0.00012',      # scan resolution robot feed axis in m
+            'concave_resolution':           '0.0005',       # hull resolution
+            'filter_down_size':             '0.0002',       # down size on clustering
+            'belt_width_threshold':         '0.8',          # Fraction of the belt width that is the minimum width threshold for removed volume to be valid 
             }
         ]
     )
